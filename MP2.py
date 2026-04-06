af60cb1ae41e7b23dfe519955a83f15b2e4b668f
Hellothereobi123 <tharun.dilliraj@gmail.com>
2026-04-06 00:09:28 -0400

    automated getting AP frequencies

--- MP2.py
+++ MP2.py
@@ -1,6 +1,22 @@
 import scipy as sp
 from scipy import fft
 import numpy as np
+def get_input_data(filename):
+    new_file = open(filename, "r")
+    client_location = new_file.readline().split(" ")
+    for i in range(len(client_location)):
+        client_location[i] = float(client_location[i])
+    num_APs = int(new_file.readline())
+    ret_tuple = (client_location, num_APs)
+    for i in range(num_APs):
+        AP_location = new_file.readline().split(" ")
+        for j in range(len(AP_location)):
+            AP_location[j] = float(AP_location[j])
+        ret_tuple += (AP_location,)
+    return ret_tuple
+
+
+
 def get_AP_data(filename):
     # read data from file
     new_file = open(filename, "r")
@@ -12,14 +28,30 @@ def get_AP_data(filename):
         #print(i)
     #print(data[1])
     return np.array(new_data)
-def doppler_velocity(frequency, f0=5.8e9, direction_vect):
+def doppler_velocity(frequency, f0=5.8e9):#, unit_vector):
     c=3e8
     # calculate velocity using doppler shift formula
     return (frequency * c) / (2 * f0)
+def get_peak_frequency(num_APs, freq_array):
+    freq_list = []
+    for i in range(num_APs):
+        get_ap1 = get_AP_data(f"AP{i+1}.txt") #gets the signal data from the A{i+1}.txt file, which contains the signal data received by the reciever from the {i+1}th access point (AP)
+        print(get_ap1.shape)
+        print(get_ap1[6])
+        ap1_freq_data = sp.fft.fft(np.array(get_ap1).T) #performs a fast fourier transform on the signal data to convert it from the time domain to the frequency domain
+        ap1_mag = np.abs(ap1_freq_data) #calculates the magnitude of the frequency domain data, which is used to find the peaks in the frequency domain
+        ap1_peaks_maxval = np.argmax(ap1_mag) #finds the peak index in the frequency domain data. #converts the peaks to a numpy array for easier manipulation
+        ap1_doppler_freq = freq_array[ap1_peaks_maxval] #finds the frequency corresponding to the peak index, which is used to calculate the velocity of the reciever relative to the transmitter using the doppler shift formula
+        freq_list.append(ap1_doppler_freq)
+    return freq_list
 ### (Step 1 AP 1) ###
 
 freq_array = np.fft.fftfreq(5000000, d=0.000001) # FFT frequency array based on index
-
+input_data = get_input_data("input.txt")
+client_location = input_data[0] #tuple with client location, number of APs, and AP locations
+num_APs = input_data[1] #number of APs in the environment
+AP_locations = input_data[2:] #tuple with AP locations
+'''
 get_ap1 = get_AP_data("AP1.txt") #gets the signal data from the AP1.txt file, which contains the signal data received by the reciever from the first access point (AP)
 print(get_ap1.shape)
 print(get_ap1[6])
@@ -27,7 +59,9 @@ ap1_freq_data = sp.fft.fft(np.array(get_ap1).T) #performs a fast fourier transfo
 ap1_mag = np.abs(ap1_freq_data) #calculates the magnitude of the frequency domain data, which is used to find the peaks in the frequency domain
 ap1_peaks_maxval = np.argmax(ap1_mag) #finds the peak index in the frequency domain data. #converts the peaks to a numpy array for easier manipulation
 ap1_doppler_freq = freq_array[ap1_peaks_maxval] #finds the frequency corresponding to the peak index, which is used to calculate the velocity of the reciever relative to the transmitter using the doppler shift formula
-
+'''
+freq_list = get_peak_frequency(num_APs, freq_array)
+print(freq_list)
 
 
 # steps per AP (step 1)
